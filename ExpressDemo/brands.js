cc37ede6792934b1b964d50368b66f3899026e2c
kavi-priyas <kavipriya.s2021it@sece.ac.in>
2024-08-08 15:34:04 +0530

    first commit

---
+++ ExpressDemo/brands.js
@@ -0,0 +1,95 @@
+module.exports=[
+    {
+     brand:"Maruthi",
+    models: [
+ 
+     {id:"1",
+    model:"Maruthi ALTO 2023",
+     type:"Manual",
+     fuel:"Petrol",
+     seats:"5 seats",
+     price:" 230"},
+ 
+     {id:"2",
+     model:"Maruthi Ertiga 2019",
+     type:"Automatic",
+     fuel:"Petrol",
+     seats:"5 seats",
+     price:" 246"},
+ 
+     {id:"3",
+     model:"Maruthi celerio 2023",
+     type:"Manual",
+     fuel:"Petrol",
+     seats:"5 seats",
+     price:" 337"}
+ 
+     
+ 
+     ] 
+   },
+ 
+ 
+   {
+     brand:"Honda",
+   models:  [
+ 
+     {id:"4",
+     model:"Honda Civic 2022",
+     type:"Manual",
+     fuel:"Petrol",
+     seats:"5 seats",
+     price:" 230"},
+ 
+     {id:"5",
+     model:"Honda City 2019",
+     type:"Automatic",
+     fuel:"Petrol",
+     seats:"5 seats",
+     price:" 246"},
+ 
+     {id:"6",
+     model:"Honda BR-V 2023",
+     type:"Manual",
+     fuel:"Petrol",
+     seats:"5 seats",
+     price:" 337"}
+ 
+     
+     
+     ]  
+   },
+ 
+   {
+     brand:"Hyundai",
+   models:  [
+ 
+     {id:"7",
+     model:"Hyundai i30 2023",
+     type:"Manual",
+     fuel:"Petrol",
+     seats:"5 seats",
+     price:" 246"},
+ 
+     {id:"8",
+     model:"Hyundai EON 2019",
+     type:"Automatic",
+     fuel:"Petrol",
+     seats:"5 seats",
+     price:" 246"},
+ 
+     {id:"9",
+     model:"Hyundai i20 2020",
+     type:"Manual",
+     fuel:"Petrol",
+     seats:"5 seats",
+     price:" 246"}
+ 
+     
+     
+     ]  
+   }
+ 
+ 
+ ]
+ 
